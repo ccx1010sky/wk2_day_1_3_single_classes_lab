bc9754c73f13d7ff776d108e6d69af3e9fc63161
ccx1010sky <ccx1010@gmail.com>
2021-11-22 14:49:00 +0000

    second commit

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -10,11 +10,6 @@ class Team:
         self.players.append(new_player)
     
     def has_player(self,search_player):
-        # for player in self.players:
-        #     if player == search_player:
-        #       return True
-        # return False
-        
         return search_player in self.players
         
 
