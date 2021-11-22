462aba0e8b5ff6cee011d64e2a7c3f262c322be0
ccx1010sky <ccx1010@gmail.com>
2021-11-22 14:32:56 +0000

    first commit

---
+++ single_class_lab_start_code/team_class/src/team.py
@@ -0,0 +1,32 @@
+class Team:
+    def __init__(self,name,players,coach):
+
+        self.name = name
+        self.players = players
+        self.coach = coach
+        self.points = 0
+
+    def add_player(self,new_player):
+        self.players.append(new_player)
+    
+    def has_player(self,search_player):
+        # for player in self.players:
+        #     if player == search_player:
+        #       return True
+        # return False
+        
+        return search_player in self.players
+        
+
+
+    def play_game(self, win):
+        if win:
+           self.points += 3
+
+
+
+
+
+
+
+
